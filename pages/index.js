1d1cf3bfe859eca002d8b8237bf45322b61ce1cb
osmanagd <mrissamhassnaoui@gmail.com>
2023-08-23 13:39:14 +0100

    Your commit message

--- pages/index.js
+++ pages/index.js
@@ -6,41 +6,18 @@ const Index = () => {
   const [thumbnailOptions, setThumbnailOptions] = useState([]);
 
   const getYouTubeThumbnail = (url) => {
-    let regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
-    let match = url.match(regExp);
-
-    if (match && match[1].length === 11) {
-      const videoURL = match[1];
-      const thumbnailBaseUrl = "http://img.youtube.com/vi/";
-
-      const options = [
-        { resolution: "HD (1280x720)", code: "maxresdefault" },
-        { resolution: "SD (640x480)", code: "sddefault" },
-        { resolution: "Normal (480x360)", code: "hqdefault" },
-        { resolution: "Medium (320x180)", code: "mqdefault" },
-        { resolution: "Low (120x90)", code: "default" },
-      ];
-
-      const thumbnailOptions = options.map((option) => ({
-        resolution: option.resolution,
-        url: `${thumbnailBaseUrl}${videoURL}/${option.code}.jpg`,
-      }));
-
-      setThumbnailOptions(thumbnailOptions);
-      setVideoURL("");
-    } else {
-      setThumbnailOptions([]);
-    }
+    // ... (your existing code for fetching thumbnails)
   };
 
   return (
     <div className="container mx-auto px-4 py-8">
       <header className="text-center mb-8">
-        <h1 className="text-3xl font-bold mb-2">
-          <center>Youtube Thumbnail Downloader</center>
-        </h1>
-        <p className="text-gray-600">
-          <center>Download Youtube Thumbnail.</center>
+        <h1 className="text-3xl font-bold mb-2">YouTube Thumbnail Downloader</h1>
+		<p className="text-gray-600" align="center">
+          Youtube thumbnail Grabber.
+        </p>
+		<p className="text-gray-600" align="center">
+          Download Youtube Thumbnail.
         </p>
       </header>
       <div className="text-center">
@@ -51,31 +28,46 @@ const Index = () => {
           value={videoURL}
           onChange={(e) => setVideoURL(e.target.value)}
         />
-        <center><button
+        <button
           className="btn-blue mt-2"
           onClick={() => getYouTubeThumbnail(videoURL)}
         >
           Download Thumbnails
-        </button></center>
+        </button>
+        <p className="text-gray-600 mt-4" align="center">
+          YouTube Thumbnail Downloader Supports All YouTube Videos Including 8K, 4K, 1080p, HD, HQ, etc.
+        </p>
+        <p className="text-gray-600" align="center">
+          YouTube Thumbnail Downloader - Download YouTube Thumbnails
+        </p>
+        <p className="text-gray-600" align="center">
+          This YouTube Thumbnail Grabber is a free online tool, with the help of this YouTube Thumbnail Grabber you can easily Download YouTube Video Thumbnail in different sizes and qualities.
+        </p>
+		<p className="text-gray-600" align="center">
+        YouTube allows channel owners to post all types of engaging videos content and let you watch it. But, when it comes to the thumbnails, you are not able to zoom out or download them. With our YouTube Thumbnail Grabber, you can view full-size thumbnail images in their original quality and save them to your devices. The downloader is free and only requires you to enter a video link for the magic to happen.
+        </p>
+		<p className="text-gray-600" align="center">
+		How do I download a YouTube thumbnail?
+        </p>
+	<p className="text-gray-600" align="center">
+		1. Copy the Required YouTube Video URL/ Link from YT Website or App.
+        </p>
+		<p className="text-gray-600" align="center">
+		2. Paste the Youtube video Link / URL in the Given Input Box.
+        </p>
+		<p className="text-gray-600" align="center">
+		3. Now Click on the "Download Youtube Thumbnail" Button.
+        </p>
+		<p className="text-gray-600" align="center">
+		4. After That You will get a list of all The Available YT Video Thumbnail Qualities, Which you can Choose From High Quality(HQ),
+High Definition (HD), 4K, 720p, 1080p Thumbnail, Low Quality and Medium Quality YT Thumbnails are also available.
+        </p>
+		<p className="text-gray-600" align="center">
+		5. Click on the Download Button Which result you want, and Your YT Video Thumbnail will be Downloaded in your Devices Storage.
+        </p>
+		
       </div>
-      {thumbnailOptions.length > 0 && (
-        <div className="mt-8">
-          <h2 className="text-xl font-semibold mb-4">Thumbnail Options</h2>
-          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-            {thumbnailOptions.map((option, index) => (
-              <div key={index} className="thumbnail-option">
-                <img src={option.url} alt={`Thumbnail ${index + 1}`} />
-                <button
-                  className="btn-blue mt-2"
-                  onClick={() => copy(option.url)}
-                >
-                  Copy Image URL
-                </button>
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
+      {/* Rest of your code remains the same */}
     </div>
   );
 };
