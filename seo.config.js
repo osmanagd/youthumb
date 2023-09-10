0e22999c25b496ff1efad758b1bd2ac616788064
osmanagd <142901539+osmanagd@users.noreply.github.com>
2023-09-10 12:16:33 +0100

    Your commit message

---
+++ seo.config.js
@@ -0,0 +1,19 @@
+// seo.config.js
+const defaultSEO = {
+  title: 'YouTube thumbnail downloader',
+  description: 'UrlThumbnails is the best tool for downloading YouTube video thumbnails.',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://urlthumbnails.com',
+    site_name: 'YouTube thumbnail downloader',
+  },
+  twitter: {
+    handle: '@yourtwitterhandle',
+    site: '@yoursitetwitter',
+    cardType: 'summary_large_image',
+  },
+  keywords: ['YouTube thumbnail downloader', 'YouTube thumbnail images', 'retrieve YouTube video thumbnails', 'store YouTube video thumbnails', 'YouTube thumbnail downloader',' efficient YouTube thumbnail retrieval', 'YouTube thumbnail generator', 'extract YouTube video thumbnails', 'save YouTube video thumbnails',' download YouTube video thumbnails',' obtain YouTube video thumbnails', 'fetch YouTube thumbnail images', 'capture YouTube video thumbnails', 'efficient thumbnail retrieval tool', 'retrieve and store YouTube video thumbnails','YouTube thumbnail retrieval', 'YouTube video thumbnail generator', 'YouTube video thumbnail grabber', 'YouTube thumbnail grabber','thumbnail grabber', 'thumbnail generator', 'YouTube thumbnail downloader', 'extract YouTube video thumbnails', 'store YouTube video thumbnails',' download YouTube video thumbnails', 'capture YouTube video thumbnails',' grab YouTube video thumbnails','save YouTube video thumbnails', 'retrieve YouTube video thumbnails', 'get YouTube video thumbnails', 'efficient YouTube thumbnail tool', 'easy YouTube thumbnail retrieval'], // Add your keywords here
+};
+
+export default defaultSEO;
