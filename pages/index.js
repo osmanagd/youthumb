2b10cbeb30cdf45134ee0d3a819c6c8fe35cbadf
osmanagd <142901539+osmanagd@users.noreply.github.com>
2023-09-10 12:49:15 +0100

    Your commit message

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-// pages/index.js
 import { NextSeo } from 'next-seo';
 import defaultSEO from '../seo.config.js';
 import React, { useState } from "react"; // Import React and useState here
@@ -53,62 +52,30 @@ const Index = () => {
         </p>
       </header>
       <div className="text-center">
-        <input
-          type="text"
-          className="w-full md:w-1/2 px-4 py-2 border rounded"
-          placeholder="Enter YouTube URL"
-          value={videoURL}
-          onChange={(e) => setVideoURL(e.target.value)}
-        />
-        <button
-          className="btn-blue mt-2"
-          onClick={() => getYouTubeThumbnail(videoURL)}
-        >
-          Download Thumbnails
-        </button>
+        <div className="flex items-center justify-center"> {/* Use flex container */}
+          <input
+            type="text"
+            className="w-full md:w-1/2 px-4 py-2 border rounded"
+            placeholder="Enter YouTube URL"
+            value={videoURL}
+            onChange={(e) => setVideoURL(e.target.value)}
+          />
+          <button
+            className="btn-blue ml-2"
+            onClick={() => getYouTubeThumbnail(videoURL)}
+          >
+            Download Thumbnails
+          </button>
+        </div>
         <div className="mt-4 text-gray-600 text-left">
-          <p><strong>You will find the pictures below.</strong></p>
-          <p><strong>YouTube Thumbnail Downloader Supports All YouTube Videos Including 8K, 4K, 1080p, HD, HQ, etc.</strong></p>
-          <p><strong>YouTube Thumbnail Downloader - Download YouTube Thumbnails.</strong></p>
-          <p><strong>This YouTube Thumbnail Grabber is a free online tool, with the help of this YouTube Thumbnail Grabber you can easily Download YouTube Video Thumbnail in different sizes and qualities.
-          .</strong></p>
-          <p><strong>What is a YouTube Thumbnail Grabber?</strong></p>
-          <p>YouTube allows channel owners to post all types of engaging videos content and let you watch it. But, when it comes to the thumbnails, you are not able to zoom out or download them. With our YouTube Thumbnail Grabber, you can view full-size thumbnail images in their original quality and save them to your devices. The downloader is free and only requires you to enter a video link for the magic to happen.
-          </p>
-          <p><strong>How do I download a YouTube thumbnail?</strong></p>
-          <ol>
-            <li>Copy the Required YouTube Video URL/Link from the YT Website or App.</li>
-            <li>Paste the YouTube video Link/URL in the Given Input Box.</li>
-            <li>Click on the "Download YouTube Thumbnail" Button.</li>
-            <li>You will get a list of all the available YT Video Thumbnail Qualities, which you can choose from:</li>
-            <ul>
-              <li>High Quality (HQ)</li>
-              <li>High Definition (HD)</li>
-              <li>4K</li>
-              <li>720p</li>
-              <li>1080p Thumbnail</li>
-              <li>Low Quality</li>
-              <li>Medium Quality</li>
-            </ul>
-            <li>Click on the Download Button for the desired result, and your YT Video Thumbnail will be Downloaded to your Device's Storage.</li>
-          </ol>
+          {/* ... (rest of the code) */}
         </div>
       </div>
       {thumbnailOptions.length > 0 && (
         <div className="mt-8">
           <h2 className="text-xl font-semibold mb-4">Thumbnail Options</h2>
           <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-            {thumbnailOptions.map((option, index) => (
-              <div key={index} className="thumbnail-option">
-                <img src={option.url} alt={`Thumbnail ${index + 1}`} />
-                <button
-                  className="btn-blue mt-2"
-                  onClick={() => copy(option.url)}
-                >
-                  Copy Image URL
-                </button>
-              </div>
-            ))}
+            {/* ... (rest of the code) */}
           </div>
         </div>
       )}
